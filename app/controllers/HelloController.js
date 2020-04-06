3b264895aeefd95de4fcea776c8e00973521bd5b
Christian Dominguez <christian@redbonzai.com>
2020-04-06 08:58:32 -0500

    Changes to HelloController

--- app/controllers/HelloController.js
+++ app/controllers/HelloController.js
@@ -1,6 +1,8 @@
 export const hello = (req, res) => {
   return res.status(200).json({
-    result: 'hello ...'
+    result: `hello Sumana ... I think about you every day, and I hope and pray that you, and the children are doing well. 
+          I know that I left abruptly, and I am sorry for this.  You see, I had no choice. 
+          I was in love with you, and there was nothing that you or I could do about that.`,
   });
 };
 
